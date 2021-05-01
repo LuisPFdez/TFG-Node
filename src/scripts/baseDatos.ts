b08583a69b4558c28ab462726dbe4e95b083a5a3
Luis Puente Fernández <Luis.p.fdez9@gmail.com>
2021-05-01 22:41:24 +0200

    Añadido script para la carga inicial

---
+++ src/scripts/baseDatos.ts
@@ -0,0 +1,52 @@
+/**
+ * @file Script que permite hacer una carga inicial en la base de datos
+ * @author Luis Puente Fernández
+ * 
+*/
+
+import credenciales from "../config/ConfigDB.json";
+import admin, { ServiceAccount } from 'firebase-admin';
+import ObjetoUsuarioInterface, { } from "../interfaces/ObjetoUsuarioInterface";
+import { Usuario } from "../model/Usuario";
+import config from "../config/Config.json";
+
+admin.initializeApp({
+    credential: admin.credential.cert(<ServiceAccount>credenciales)
+});
+
+const UsuariosDB = (admin.firestore()).collection(config.coleccion);
+
+//Es necesario que los codigos del usuario no tengan espacios, para evitar problemas con la aplicacion.
+const DatosUsuarios: Array<ObjetoUsuarioInterface> = [
+    {
+        codUsuario: "Admin",
+        descripcion: "Administrador",
+        password: "paso",
+        tipo: "ADMIN"
+    },
+    {
+        codUsuario: "Pruebas",
+        descripcion: "Usuario de pruebas",
+        password: "paso",
+        tipo: "USUARIO"
+    },
+    {
+        codUsuario: "Pruebas2",
+        descripcion: "Usuario de pruebas 2",
+        password: "paso",
+        tipo: "USUARIO"
+    }
+];
+
+DatosUsuarios.forEach(async (datos): Promise<void> => {
+    const usuario: ObjetoUsuarioInterface = datos;
+
+    //Se encripta la contraseña
+    usuario.password = Usuario.encriptarPassword(datos.codUsuario, datos.password);
+
+
+    await UsuariosDB.doc(usuario.codUsuario).set(usuario)
+        .catch(error => { console.log(error) });
+});
+
+console.log("\x1b[34m", "Datos insertados", "\x1b[0m");
